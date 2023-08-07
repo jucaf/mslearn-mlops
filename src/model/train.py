c715eb50fad7ddddd0184490b22ccc7b03c8bbe9
Juan Carlos Fernandez <jcfernandez@keedio.com>
2023-08-07 10:44:04 +0200

    Add new Lines to the file

--- src/model/train.py
+++ src/model/train.py
@@ -43,6 +43,7 @@ def split_data(df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
+
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
